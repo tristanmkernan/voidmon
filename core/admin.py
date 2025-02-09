6f649bed3d39736a3a843ff704ea4c4b2d329085
Tristan Kernan <tristanmkernan@gmail.com>
2025-02-09 16:06:59 -0500

    feat: improve admin

--- core/admin.py
+++ core/admin.py
@@ -7,12 +7,18 @@ from .models import NotificationSubscription, Scan, ScanIssue
 class ScanAdmin(admin.ModelAdmin):
     list_display = ("url", "status", "created_at")
 
+    readonly_fields = ("uuid", "created_at")
+
 
 @admin.register(NotificationSubscription)
 class NotificationSubscriptionAdmin(admin.ModelAdmin):
     list_display = ("url", "email", "created_at")
 
+    readonly_fields = ("uuid", "created_at")
+
 
 @admin.register(ScanIssue)
 class ScanIssueAdmin(admin.ModelAdmin):
     list_display = ("scan__uuid", "type", "created_at")
+
+    readonly_fields = ("uuid", "created_at")
